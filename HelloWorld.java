38bcdca2381afb0645c6dd7e35967f22867786db
xlxgoodguy <39412417+xlxgoodguy@users.noreply.github.com>
2019-01-22 09:06:59 +0800

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,6 @@
 class HelloWorld{
 	public static void main(String[] str){
 		System.out.println("Hello World!");
-		if (true){
-			return ;
-		}
 		System.out.println("This is a big change!");
 	}
 }
