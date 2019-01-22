0364051071f9ccd4cbf841a2479c03714a54ee03
xlxgoodguy <39412417+xlxgoodguy@users.noreply.github.com>
2019-01-22 09:02:15 +0800

    bad
    bad

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 class HelloWorld{
-	public static void main(String[] str){
+	public static void main1(String[] str){
 		System.out.println("Hello World!");
 		System.out.println("This is a change!");
 	}
